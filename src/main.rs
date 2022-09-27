9e84d0364da5231113d46c872c908918392c41ac
Arkadii Berezkin <arkberezkin@ip-192-168-1-14.ec2.internal>
2022-09-27 20:22:21 +0200

    implementation

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use hello::{HelloRequest, HelloResponse, hello_service_server::{HelloServiceServer, HelloService}};
+
+pub mod hello {
+    tonic::include_proto!("hello");
+}
+
+#[derive(Debug, Default)]
+pub struct HelloServiceImplementation {}
+
+#[tonic::async_trait]
+impl HelloService for HelloServiceImplementation {
+  async fn hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
+    let r = request.into_inner();
+
+    Ok(Response::new(hello::HelloResponse {
+        message: format!("Hello, {}!", r.name)
+    }))
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "[::1]:8080".parse().unwrap();
+    let hello_service = HelloServiceImplementation::default();
+
+    Server::builder().add_service(HelloServiceServer::new(hello_service))
+        .serve(address)
+        .await?;
+
+    Ok(())
+}
+
